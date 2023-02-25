fa85aa2a054dc4392f36434e678f563cbf54ccee
pulgueta <retardix456@gmail.com>
2023-02-25 17:39:39 -0500

    Fix

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -3,9 +3,8 @@ import { FC } from 'react'
 import { LazyLoadImage } from 'react-lazy-load-image-component';
 import { Button, ButtonGroup, Card, CardBody, Divider, Heading, Stack, Text } from '@chakra-ui/react'
 import { priceFormat } from '../utils';
-import { ProductInformation } from '../interfaces';
 
-const ProductCard: FC<ProductInformation> = ({ image, title, description, price }): JSX.Element => {
+const ProductCard: FC = (): JSX.Element => {
     return (
         <Card maxW={['xs', 'sm', 'md']} borderRadius='xl'>
             <CardBody>
@@ -28,24 +27,6 @@ const ProductCard: FC<ProductInformation> = ({ image, title, description, price 
                 </ButtonGroup>
             </CardBody>
         </Card>
-        // <Card maxW={['xs', 'sm', 'md']} borderRadius='xl'>
-        //     <CardBody>
-        //         <LazyLoadImage
-        //             src={image}
-        //             effect='blur'
-        //             style={{ borderRadius: 12 }}
-        //         />
-        //         <Stack spacing='3' my='3'>
-        //             <Heading size='lg'>{title} SSSSSSSSS</Heading>
-        //             <Text>{description} XSSSSSSSSSSSS</Text>
-        //         </Stack>
-        //         <Divider />
-        //         <ButtonGroup mt='3'>
-        //             <Button colorScheme='telegram'>Editar</Button>
-        //             <Button colorScheme='red'>Eliminar</Button>
-        //         </ButtonGroup>
-        //     </CardBody>
-        // </Card>
     )
 }
 
