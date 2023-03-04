fee89790a02d9a7dabc2b82f2e660d283bea01f0
pulgueta <retardix456@gmail.com>
2023-03-04 03:14:22 -0500

    Fix

--- src/pages/admin/products/Products.tsx
+++ src/pages/admin/products/Products.tsx
@@ -1,12 +1,12 @@
 import { FC } from 'react'
 
 import { Center, GridItem, Heading, Text, VStack, Grid, ButtonGroup, Button } from '@chakra-ui/react'
+import { FaArrowLeft, FaArrowRight } from 'react-icons/fa'
 
 import { ProductCard } from '../../../components/admin'
 import { ProductCardSkeleton } from '../../../components/skeleton'
-import { useProducts } from '../../../hooks'
 import { ProductInformation } from '../../../interfaces'
-import { FaArrowLeft, FaArrowRight } from 'react-icons/fa'
+import { useProducts } from '../../../hooks'
 
 const Products: FC = (): JSX.Element => {
     const { handleNextProd, handlePrevProd, loading, more, products } = useProducts();
@@ -24,32 +24,32 @@ const Products: FC = (): JSX.Element => {
                         :
                         products.length !== 0
                             ?
-                                <VStack gap={6}>
-                                    <Grid gap={6} templateColumns={['repeat(1, 1fr)', 'repeat(1, 1fr)', 'repeat(2, 1fr)', 'repeat(2, 1fr)', 'repeat(3, 1fr)']}>
-                                        {products.slice(more - 3, more).map(({ category, description, id, image, price, title }: ProductInformation) => {
-                                            return (
-                                                <GridItem key={id}>
-                                                    <ProductCard
-                                                        id={id}
-                                                        category={category}
-                                                        description={description}
-                                                        image={image}
-                                                        price={price}
-                                                        title={title}
-                                                    />
-                                                </GridItem>
-                                            )
-                                        })}
-                                    </Grid>
-                                    {
-                                        products.length >= 4 && (
-                                            <ButtonGroup mt={8}>
-                                                <Button onClick={handlePrevProd} colorScheme='blue' leftIcon={<FaArrowLeft size='0.75em' />}>Anterior</Button>
-                                                <Button onClick={handleNextProd} colorScheme='green' rightIcon={<FaArrowRight size='0.75em' />}>Siguiente</Button>
-                                            </ButtonGroup>
+                            <VStack gap={6}>
+                                <Grid gap={6} templateColumns={['repeat(1, 1fr)', 'repeat(1, 1fr)', 'repeat(2, 1fr)', 'repeat(2, 1fr)', 'repeat(3, 1fr)']}>
+                                    {products.slice(more - 3, more).map(({ category, description, id, image, price, title }: ProductInformation) => {
+                                        return (
+                                            <GridItem key={id}>
+                                                <ProductCard
+                                                    id={id}
+                                                    category={category}
+                                                    description={description}
+                                                    image={image}
+                                                    price={price}
+                                                    title={title}
+                                                />
+                                            </GridItem>
                                         )
-                                    }
-                                </VStack>
+                                    })}
+                                </Grid>
+                                {
+                                    products.length >= 4 && (
+                                        <ButtonGroup mt={8}>
+                                            <Button onClick={handlePrevProd} colorScheme='blue' leftIcon={<FaArrowLeft size='0.75em' />}>Anterior</Button>
+                                            <Button onClick={handleNextProd} colorScheme='green' rightIcon={<FaArrowRight size='0.75em' />}>Siguiente</Button>
+                                        </ButtonGroup>
+                                    )
+                                }
+                            </VStack>
                             :
                             <Text fontWeight='medium'>No hay productos 😓</Text>
                 }
